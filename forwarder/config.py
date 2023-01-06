14cb58698f223ab6f5bf67f6f49e6df79bfb6e27
19521790 <80632602+19521790@users.noreply.github.com>
2023-01-06 17:39:30 +0700

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -4,8 +4,8 @@ class Development(Config):
     API_KEY = "5727416332:AAFY-yxFtLf1T0jGUyt4aLHfVUVqF0BizDQ"  # Your bot API key
     OWNER_ID = 2111357106  # Your user id
  
-    FROM_CHATS = [-1001473494312]  # List of chat id's to forward messages from.
-    TO_CHATS = [-1001735866288]  # List of chat id's to forward messages to.
+    FROM_CHATS = [-1001735866288]  # List of chat id's to forward messages from.
+    TO_CHATS = [-1001155176622]  # List of chat id's to forward messages to.
 
     REMOVE_TAG = True
     WORKERS = 4

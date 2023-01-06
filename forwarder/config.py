5bcaed53bf8dc3c547a7c5d2308617d25c82d4f1
19521790 <80632602+19521790@users.noreply.github.com>
2023-01-06 17:15:50 +0700

    update redirect

--- forwarder/config.py
+++ forwarder/config.py
@@ -4,8 +4,8 @@ class Development(Config):
     API_KEY = "5505684344:AAE98Odd40aqZiIAi8CtmtDUcfjdqxaompk"  # Your bot API key
     OWNER_ID = 2111357106  # Your user id
  
-    FROM_CHATS = [-1001642572065]  # List of chat id's to forward messages from.
-    TO_CHATS = [-1001112294077]  # List of chat id's to forward messages to.
+    FROM_CHATS = [-1001473494312]  # List of chat id's to forward messages from.
+    TO_CHATS = [-1001155176622]  # List of chat id's to forward messages to.
 
     REMOVE_TAG = True
     WORKERS = 4

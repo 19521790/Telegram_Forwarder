d4c7ac294cf8ea269f0fc615e006fd913a25d2e9
19521790 <80632602+19521790@users.noreply.github.com>
2023-01-06 17:28:53 +0700

    Update config.py

--- forwarder/config.py
+++ forwarder/config.py
@@ -1,7 +1,7 @@
 from forwarder.sample_config import Config
 
 class Development(Config):
-    API_KEY = "5505684344:AAE98Odd40aqZiIAi8CtmtDUcfjdqxaompk"  # Your bot API key
+    API_KEY = "5727416332:AAFY-yxFtLf1T0jGUyt4aLHfVUVqF0BizDQ"  # Your bot API key
     OWNER_ID = 2111357106  # Your user id
  
     FROM_CHATS = [-1001473494312]  # List of chat id's to forward messages from.
